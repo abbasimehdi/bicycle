2c49fc72d443e6efd36eafbac5857465a935a6fc
root <abbasimehdi24@gmaile.com>
2024-01-31 15:10:59 +0330

    add provider

---
+++ app/Modules/Domain/profile/src/ProfileServiceProvider.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Bicycle\Modules\Domain\Profile;
+
+use Bicycle\Modules\Domain\Profile\Contracts\ProfileInterface;
+use Bicycle\Modules\Domain\Profile\Models\Schemas\Constants\ProfileConstants;
+use Bicycle\Modules\Domain\Profile\Services\ProfileService;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\ServiceProvider;
+
+class ProfileServiceProvider extends ServiceProvider
+{
+    /**
+     * @return void
+     */
+    public function boot(): void
+    {
+        $this->routeRegister();
+        $this->loadMigrationsFrom(__DIR__.ProfileConstants::MIGRATION_ROUTE);
+        $this->app->bind(ProfileInterface::class, ProfileService::class);
+    }
+
+    /**
+     * @return void
+     */
+    private function routeRegister(): void
+    {
+        Route::prefix(ProfileConstants::PREFIX)
+            ->namespace(ProfileConstants::CONTROLLER_ROUTE)
+            ->group(__DIR__.ProfileConstants::API_ROUTE);
+    }
+
+}

aadacf346757430021d03dc3890bc299958b9197
root <abbasimehdi24@gmaile.com>
2024-02-01 15:28:24 +0330

    Add middleware to check user admin

---
+++ app/Http/Middleware/CheckUserIsAdmin.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Bicycle\Modules\Domain\Core\Exceptions\CustomException;
+use Closure;
+use Exception;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class CheckUserIsAdmin
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param Closure(Request): (Response) $next
+     * @throws Exception
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        if (
+            auth('api')->check() &&
+            auth('api')->user()->hasRole('administrator')
+        ) {
+            return $next($request);
+        }
+
+        return (new CustomException())->message(
+            'You do not have permission to access this page',
+            Response::HTTP_FORBIDDEN);
+
+        throw new \InvalidArgumentException('You do not have permission to access this page', 403);
+    }
+}
